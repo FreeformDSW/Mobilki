fe8f8ac28e944220d65adf5fcac17cb91d8e3e34
FreeformDSW <133800843+FreeformDSW@users.noreply.github.com>
2024-01-28 18:57:34 +0100

    Dodanie komentarzy do elementów

--- main.dart
+++ main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart'; // biblioteka do zapisania daty w lepszym formacie
 
-void main() {
+void main() { // uruchomienie appki
   runApp(MyApp());
 }
 
-class Transakcja {
+class Transakcja { // zbudowanie transakcji
   final String title;
   final double amount;
   final DateTime timestamp;
@@ -13,7 +13,7 @@ class Transakcja {
   Transakcja({required this.title, required this.amount, required this.timestamp});
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { // core appki
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,27 +22,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Bilans extends StatefulWidget {
+class Bilans extends StatefulWidget { // główny ekran po updacie bilansu
   @override
   BilansState createState() => BilansState();
 }
 
-class BilansState extends State<Bilans> {
+class BilansState extends State<Bilans> { // stan
   double accountBalance = 0.0;
   List<Transakcja> transactionHistory = [];
 
-  void addTransaction(String title, double amount) {
+  void addTransaction(String title, double amount) { // funkcja odpowiedzialna za dodanie transakcji
     setState(() {
       accountBalance += amount;
       transactionHistory.insert(0, Transakcja(title: title, amount: amount, timestamp: DateTime.now()));
     });
   }
 
-  Color mainColor() {
+  Color mainColor() { // funkcja do zmiany koloru tła zależnie od stanu konta użytkownika
     return accountBalance >= 0 ? Colors.green[100]! : Colors.red[100]!;
   }
 
-  @override
+  @override //wygląd appki i poszczególne elementy
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
@@ -54,7 +54,7 @@ class BilansState extends State<Bilans> {
           ),
           centerTitle: true,
           backgroundColor: Colors.indigo,
-          bottom: TabBar(
+          bottom: TabBar( // stworzenie dwóch zakładek: główną i historie transakcji
             tabs: [
               Tab(
                 child: Text(
@@ -69,7 +69,7 @@ class BilansState extends State<Bilans> {
                 ),
               ),
             ],
-            indicatorColor: Colors.white,
+            indicatorColor: Colors.white, // biały indykator lepiej wygląda
           ),
         ),
         body: Container(
@@ -81,20 +81,20 @@ class BilansState extends State<Bilans> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text(
-                      'Stan Konta: ${accountBalance.toStringAsFixed(2)} zł',
+                      'Stan Konta: ${accountBalance.toStringAsFixed(2)} zł', // Stan Konta
                       style: TextStyle(
                         fontSize: 32.0,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    ElevatedButton(
+                    ElevatedButton( // guzik
                       onPressed: () {
                         showDialog(
                           context: context,
                           builder: (BuildContext context) {
                             String title = '';
                             double amount = 0.0;
-                            return AlertDialog(
+                            return AlertDialog( //panel do wprowadzenia kwoty
                               title: Text('Dodaj kwotę'),
                               content: Column(
                                 children: [
@@ -111,7 +111,7 @@ class BilansState extends State<Bilans> {
                                     keyboardType: TextInputType.numberWithOptions(decimal: true),
                                     onChanged: (value) {
                                       if (value.isNotEmpty) {
-                                        double parsedAmount = double.tryParse(value) ?? 0.0;
+                                        double parsedAmount = double.tryParse(value) ?? 0.0; // sprawdzenie co się wpisało
                                         setState(() {
                                           amount = parsedAmount;
                                         });
@@ -145,7 +145,7 @@ class BilansState extends State<Bilans> {
                   ],
                 ),
               ),
-              ListView.separated(
+              ListView.separated( // historia transakcji
                 itemCount: transactionHistory.length,
                 separatorBuilder: (context, index) => Divider(),
                 itemBuilder: (context, index) {
